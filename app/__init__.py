88c39937416411c6a2b647e31c4712771edd36fa
Ashis Chandra Das <ashis103@gmail.com>
2025-03-08 22:50:47 +0600

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from app.database import init_db
+
+app = Flask(__name__)
+
+if os.getenv('FLASK_ENV') != 'test':
+    init_db()
+
+# else:
+#     app.config.from_object('app.config')
+
+from app import routes
+
+
